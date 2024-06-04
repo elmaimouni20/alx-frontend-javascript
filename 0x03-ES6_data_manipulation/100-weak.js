5f9a49323bb34646ec0f09df632df2b1d63411a8
el_maimouni <106943807+elmaimouni20@users.noreply.github.com>
2024-06-04 11:02:31 +0100

    commit

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,4 +1,10 @@
 export const weakMap = new WeakMap();
-export function queryAPI(object) {
-  
+export function queryAPI(endpoint) {
+    let count = endpointMap.get(endpoint) || 0;
+    count++;
+    endpointMap.set(endpoint, count);
+
+    if (count >= 5) {
+        throw new Error('Endpoint load is high');
+    }
 }
