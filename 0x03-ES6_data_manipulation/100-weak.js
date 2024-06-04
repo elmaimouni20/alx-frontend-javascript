3c7707415972281ef504e69a37c28e4e592f1a4b
el_maimouni <106943807+elmaimouni20@users.noreply.github.com>
2024-06-04 11:14:50 +0100

    co

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,13 @@
 export const weakMap = new WeakMap();
 
-export function queryAPI(endpoint) {
-    let count = weakMap.get(endpoint) || 0;
-    count++;
-    weakMap.set(endpoint, count);
 
-    if (count >= 5) {
-        throw new Error('Endpoint load is high');
-    }
+export function queryAPI(endpoint) {
+  let count = weakMap.get(endpoint) || 0;
+  count += 1;
+
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  } else {
+    weakMap.set(endpoint, count);
+  }
 }
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,16 +1,19 @@
 export default function cleanSet(set, startString) {
-  if (!startString) {
-    return '';
-  } else if (set instanceof Set) {
-    const result = [];
-    set.forEach((element) => {
-      if (element.startsWith(startString)) {
-        result.push(element.substring(startString.length));
-      }
-    });
+  const list = [];
 
-    return result.join('-');
-  } else {
+  if (
+    typeof set !== 'object'
+    || typeof startString !== 'string'
+    || startString.length === 0
+  ) {
     return '';
   }
+
+  for (const item of set) {
+    if (item && item.startsWith(startString)) {
+      list.push(item.slice(startString.length));
+    }
+  }
+
+  return list.join('-');
 }
