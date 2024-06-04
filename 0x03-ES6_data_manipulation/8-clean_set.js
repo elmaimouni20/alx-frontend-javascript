73e8898393ede496e38fb5ff8fcaf0d04602b34d
el_maimouni <106943807+elmaimouni20@users.noreply.github.com>
2024-06-04 11:11:49 +0100

    c

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,16 @@
-function cleanSet(set, startString) {
-  let result = '';
-
-  for (let value of set) {
-      if (value.startsWith(startString)) {
-          result += '-' + value.slice(startString.length);
+export default function cleanSet(set, startString) {
+  if (!startString) {
+    return '';
+  } else if (set instanceof Set) {
+    const result = [];
+    set.forEach((element) => {
+      if (element.startsWith(startString)) {
+        result.push(element.substring(startString.length));
       }
-  }
+    });
 
-  return result.startsWith('-') ? result.slice(1) : result;
+    return result.join('-');
+  } else {
+    return '';
+  }
 }
