62bfc4c815db6310759d576a64ff481b59499c0f
el_maimouni <106943807+elmaimouni20@users.noreply.github.com>
2024-06-05 15:40:10 +0100

    remove space

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,5 @@
 export default function cleanSet(set, startString) {
   const list = [];
-
   if (
     typeof set !== 'object'
     || typeof startString !== 'string'
@@ -8,12 +7,10 @@ export default function cleanSet(set, startString) {
   ) {
     return '';
   }
-
   for (const item of set) {
     if (item && item.startsWith(startString)) {
       list.push(item.slice(startString.length));
     }
   }
-
   return list.join('-');
 }
